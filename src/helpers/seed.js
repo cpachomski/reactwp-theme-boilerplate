f9ae77ddd228141449e92da29d5eac85da440052
Chris <cpachomski@gmail.com>
2016-06-09 15:07:38 -0400

    create src and add seed file to test components

---
+++ src/helpers/seed.js
@@ -0,0 +1,110 @@
+module.exports = [
+  {
+    "_id": "5759be19c20c145ea60d7967",
+    "index": 0,
+    "isActive": true,
+    "picture": "http://placehold.it/150x150",
+    "age": 33,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Viola",
+      "last": "Tanner"
+    }
+  },
+  {
+    "_id": "5759be19f95d9b7dda1136b7",
+    "index": 1,
+    "isActive": false,
+    "picture": "http://placehold.it/150x150",
+    "age": 26,
+    "eyeColor": "green",
+    "name": {
+      "first": "Jerry",
+      "last": "Holder"
+    }
+  },
+  {
+    "_id": "5759be19d9e6a87c937362dd",
+    "index": 2,
+    "isActive": true,
+    "picture": "http://placehold.it/150x150",
+    "age": 40,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Pittman",
+      "last": "Weeks"
+    }
+  },
+  {
+    "_id": "5759be191cfd0ee099e68afd",
+    "index": 3,
+    "isActive": true,
+    "picture": "http://placehold.it/150x150",
+    "age": 27,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Ramos",
+      "last": "Reed"
+    }
+  },
+  {
+    "_id": "5759be19a69a7fa200dfefdf",
+    "index": 4,
+    "isActive": true,
+    "picture": "http://placehold.it/150x150",
+    "age": 23,
+    "eyeColor": "blue",
+    "name": {
+      "first": "Holly",
+      "last": "Koch"
+    }
+  },
+  {
+    "_id": "5759be19db9082f861e3ae75",
+    "index": 5,
+    "isActive": false,
+    "picture": "http://placehold.it/150x150",
+    "age": 40,
+    "eyeColor": "blue",
+    "name": {
+      "first": "Fry",
+      "last": "Wall"
+    }
+  },
+  {
+    "_id": "5759be196078c6229100abee",
+    "index": 6,
+    "isActive": true,
+    "picture": "http://placehold.it/150x150",
+    "age": 22,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Pat",
+      "last": "Knight"
+    }
+  },
+  {
+    "_id": "5759be19ef3aa42e12d88313",
+    "index": 7,
+    "isActive": false,
+    "picture": "http://placehold.it/150x150",
+    "age": 39,
+    "eyeColor": "green",
+    "name": {
+      "first": "Juarez",
+      "last": "Mcleod"
+    }
+  },
+  {
+    "_id": "5759be1951ece991e7292c07",
+    "index": 8,
+    "isActive": true,
+    "picture": "http://placehold.it/150x150",
+    "age": 26,
+    "eyeColor": "brown",
+    "name": {
+      "first": "Wilcox",
+      "last": "Colon"
+    }
+  }
+]
